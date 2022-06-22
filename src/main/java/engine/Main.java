c2a37976cd56189021a6ce9bd603bd415a2208dd
spiderbait <dontwakeupbabys@icloud.com>
2022-06-22 23:38:04 +0800

    base

--- src/main/java/engine/Main.java
+++ src/main/java/engine/Main.java
@@ -1,5 +1,20 @@
 package engine;
 
-public class Main {
+import constant.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("BOS Monitor starting...");
+        try {
+            while (true) {
+                Thread.sleep(Constant.POLLING_INTERVAL_IN_MS);
+            }
+        } catch (InterruptedException e) {
+            logger.info("BOS Monitor terminated.");
+        }
+    }
 }
